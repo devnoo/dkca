7cd3d311b7085507057df077450c946a209a7a1b
devnoo <jgdenoo@gmail.com>
2012-11-02 08:14:55 +0100

    table view working

--- de Kunstkijker/de Kunstkijker/DKAQueryResultController.m
+++ de Kunstkijker/de Kunstkijker/DKAQueryResultController.m
@@ -9,7 +9,7 @@
 #import "DKAQueryResultController.h"
 #import "DKAViewController.h"
 #import "DKAArtWorkDao.h"
-#import "DKAQueryResultTableViewCell.h"
+
 #import "DKAArtWork.h"
 
 @interface DKAQueryResultController ()
@@ -33,8 +33,7 @@
 
 - (void)viewDidLoad
 {
-    [self.tableView registerClass:[DKAQueryResultTableViewCell class] forCellReuseIdentifier:@"queryTableCell"];
-    self.artWorks = [DKAArtWorkDao getMyArtworks];
+        self.artWorks = [DKAArtWorkDao getMyArtworks];
     [super viewDidLoad];
 
     // Uncomment the following line to preserve selection between presentations.
@@ -64,70 +63,23 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *cellIdentifier = @"queryTableCell";
-    DKAQueryResultTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
-    if (cell == nil){
-        cell = [[DKAQueryResultTableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier: cellIdentifier];
+    static NSString *CellIdentifier = @"queryTableCell";
+       UITableViewCell *cell = [tableView
+                             dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle
+                                      reuseIdentifier:CellIdentifier];
     }
+   
     // Configure the cell...
     DKAArtWork *artwork = [artWorks objectAtIndex:[indexPath row]];
-    cell.image.image = artwork.photo;
-    cell.titleLabel.text = artwork.title;
-    cell.artistLabel.text = artwork.artist;
-    
+    cell.imageView.image = artwork.photo;
+    cell.textLabel.text = artwork.title;
+    cell.detailTextLabel.text = artwork.artist;
+   
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
-}
 
 @end
